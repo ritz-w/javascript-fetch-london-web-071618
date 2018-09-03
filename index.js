615664d408c48a5a7b60d7407ae28be2d97be68b
ritz-w <ritz.wu@outlook.com>
2018-09-03 11:40:56 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const app = "I don't do much.";
+const token = "9c60a38aa85e817abc957ec49f01202de396552f";
+fetch('https://api.github.com/user/repos', {
+  headers: {
+  Authorization: `token ${token}`
+  }
+}).
+  then(res => res.json()).
+  then(json => console.log(json))
